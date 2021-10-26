5d0f26cd6dee723b2f8b4cc55446b51c229d58a1
Ihor <igormykhno@gmail.com>
2021-10-26 12:04:08 +0300

    Test for check auto open

--- silpo/PrimeService.Tests/CheckoutServiceTest.cs
+++ silpo/PrimeService.Tests/CheckoutServiceTest.cs
@@ -28,6 +28,19 @@ namespace PrimeService.Tests
 
             Assert.Equal(check.getTotalCost(), 10);
         }
+        [Fact]
+        void addProduct__whenCheckIsClosed__opensNewCheck(){
+                CheckoutService checkoutService = new CheckoutService();
+            checkoutService.openCheck();
+
+            checkoutService.addProduct(new Product(7, "Milk"));
+            Check milkCheck = checkoutService.closeCheck();
+            Assert.Equal(milkCheck.getTotalCost(), 7);
+
+            checkoutService.addProduct(new Product(3, "Bred"));
+            Check breadCheck = checkoutService.closeCheck();
+            Assert.Equal(breadCheck.getTotalCost(), 3);
+        }
     }
 
 }
